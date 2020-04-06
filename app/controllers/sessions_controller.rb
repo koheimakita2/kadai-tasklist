b290613fb4e7ec614223d1c2aca4fab1ab887197
makikou <ma8618328@gmail.com>
2020-04-06 08:48:58 +0000

    tasklist

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     
     if login(email, password)
       flash[:success] = 'ログインに成功しました'
-      redirect_to tasks_path
+      redirect_to root_path
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
